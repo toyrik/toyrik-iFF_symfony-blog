ded433809a8a1a533c453b58d8d51cd6c5e19b6e
toyrik <toyrik@gmail.com>
2020-08-29 19:49:27 +0300

    Меняем роутинг для статей

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/article")
+ * @Route("/articles")
  */
 class ArticleController extends AbstractController
 {
