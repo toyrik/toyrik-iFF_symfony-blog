4722a250d85ae36462ac8f5c504ec4b8a1238b6b
toyrik <toyrik@gmail.com>
2020-08-29 22:15:14 +0300

    Добавляем вывод статей на главную страницу

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,18 +4,24 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public $pageTitle = 'Домашняя страница';
+    
     /**
      * @Route("/", name="home")
      * @return Response
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render('app/home.html.twig');
+        return $this->render('home/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+            'pageTitle' => $this->pageTitle
+        ]);
     }
 }
